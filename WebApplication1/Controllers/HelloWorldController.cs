b59e3e4b89b2b03673175945a30901c0bc82019c
Deblight <lara42956@edu.ucl.dk>
2023-02-12 12:49:57 +0100

    Add project files.

---
+++ WebApplication1/Controllers/HelloWorldController.cs
@@ -0,0 +1,59 @@
+﻿
+
+
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace WebApplication1.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        /*public string Index()
+        {
+            return "This is my default action...";
+        }*/
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Welcome(String name, int numTimes=1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        /*public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }*/
+
+        // GET: /HelloWorld/Welcome/ 
+        // Requires using System.Text.Encodings.Web;
+        public string Welcome2(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+
+        public string Welcome3(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+
+
+    }
+}
+
+
